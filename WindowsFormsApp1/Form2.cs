c05920e89bc60ae63a483724d048c707c475f9c1
Nirmal Kumar Sekar <nxsekar1@shockers.wichita.edu>
2022-06-24 12:02:47 -0500

    needs to be implemented more --> this is computer vision model

--- WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/Form2.cs
@@ -9,11 +9,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
 
 namespace WindowsFormsApp1
 {
     public partial class Form2 : Form
     {
+
+        Image<Bgr, byte> imgInput;
         public Form2()
         {
             InitializeComponent();
@@ -26,13 +32,18 @@ namespace WindowsFormsApp1
             if(imagebr.ShowDialog() == DialogResult.OK)
             {
                 recImage.Image = Image.FromFile(imagebr.FileName);
+                
+                
             }
 
         }
 
+
         private void button1_Click(object sender, EventArgs e)
         {
-         
+            VectorOfVectorOfPoint Contours = new VectorOfVectorOfPoint();
+            Mat m = new Mat();
+            CvInvoke.FindContours(recImage, Contours, m, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
         }
     }
 }
